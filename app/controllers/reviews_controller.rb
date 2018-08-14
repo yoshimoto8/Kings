d54053e482292053b002b5e6b17491af50cebe5d
吉本光輝 <yoyoyo427435@gmail.com>
2018-08-14 16:53:16 +0900

    commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,38 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def create_page
-    @item_title = params[:item_title]
-    @item_image_url = params[:item_image]
-    @review = Review.new
+    if user_signed_in?
+      #この変もしたし
+      @item_title = params[:item_title]
+      @item_image_url = params[:item_image]
+      @review = Review.new
+    else
+      redirect_to "/users/sign_in"
+    end
   end
 
   def create
     #リファクタリングしたし 
-    review_params = get_review_params
-    point = point(review_params[:reviews_valuation])
-    Review.create(reviews_valuation: point, title: review_params[:title], item_comment: review_params[:item_comment],
-                  item_image: review_params[:item_image_url], item_name: review_params[:item_title])
+    @review = get_review_params
+    if @review.save
+      redirect_to root_url
+    else
+      redirect_to root_url
+    end
   end
 
   private
 
   def get_review_params
-    params.require(:review).permit(:title, :reviews_valuation, :item_comment, :item_image_url, :item_title)
+    return set_review_params(params.require(:review).permit(:title, :reviews_valuation, :item_comment, :item_image_url, :item_title))
+  end
+
+  def set_review_params(review_params)
+    point = point(review_params[:reviews_valuation])
+    cosmetic = Cosmetic.find_by(title: review_params[:item_title]).id
+    current_user.reviews.build(cosmetics_id: cosmetic, reviews_valuation: point, title: review_params[:title], item_comment: review_params[:item_comment],
+                               item_image: review_params[:item_image_url], item_name: review_params[:item_title])
   end
 
   def point(point)
