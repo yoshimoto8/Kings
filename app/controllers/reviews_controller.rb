a9a69f9ef8a5e8487cf7f3126752ce8858e894c2
吉本光輝 <yoyoyo427435@gmail.com>
2018-08-15 16:01:32 +0900

    cosmeticsの登録するタイミングを変えた

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,7 @@ class ReviewsController < ApplicationController
       #この変もしたし
       @item_title = params[:item_title]
       @item_image_url = params[:item_image]
+      @cosmetics_url = params[:cosmetics_url]
       @review = Review.new
     else
       redirect_to "/users/sign_in"
@@ -13,7 +14,9 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @review = current_user.reviews.build(get_review_params.hash)
+    review = get_review_params
+    Cosmetic.new.insert_item(cosmetics_hash(review))
+    @review = current_user.reviews.build(review_parameter(review).hash)
     if @review.save
       redirect_to root_url
     else
@@ -24,10 +27,14 @@ class ReviewsController < ApplicationController
   private
 
   def get_review_params
-    return Review_parameter.new(params.require(:review).permit(:title, :reviews_valuation, :item_comment, :item_image_url, :item_title, :review_image))
+    params.require(:review).permit(:title, :reviews_valuation, :item_comment, :item_image_url, :item_title, :review_image, :cosmetics_url)
   end
 
-  def point(point)
-    Review.points[point]
+  def cosmetics_hash(review)
+    {title: review[:item_title], image_url: review[:item_image_url], url: review[:cosmetics_url]}
+  end
+
+  def review_parameter(review)
+    Review_parameter.new(review)
   end
 end
