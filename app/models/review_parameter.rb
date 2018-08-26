3b213260133de8c348b9c5e2d3cf85308ecec771
吉本光輝 <yoyoyo427435@gmail.com>
2018-08-26 23:01:53 +0900

    refactor review_parameter

--- app/models/review_parameter.rb
+++ app/models/review_parameter.rb
@@ -12,9 +12,9 @@ class Review_parameter
   end
 
   def hash
-    {reviews_valuation: @reviews_valuation, cosmetic_id: @cosmetic_id,
-     title: @title, item_comment: @item_comment, item_image: @item_image,
-     item_name: @item_name, review_image: @review_image}
+    {reviews_valuation: reviews_valuation, cosmetic_id: cosmetic_id,
+     title: title, item_comment: item_comment, item_image: item_image,
+     item_name: item_name, review_image: review_image}
   end
 
   private
