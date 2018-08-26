31c15477ecab5e4093c5e4158ffbac8de3e93e1b
吉本光輝 <yoyoyo427435@gmail.com>
2018-08-26 23:04:27 +0900

    :hammer: (user) refactoring user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,16 @@ class User < ApplicationRecord
   enum genders: { man: 1, women: 2 }
 
   def is_registrated_user_detail?
-    if first_name.nil? || last_name.nil? || nick_name.nil? || age.nil? || gender.nil?
+    if is_user_parameter_nil?
       return true
     end
 
     false
+  end
+
+  private
+
+  def is_user_parameter_nil?
+    first_name.nil? || last_name.nil? || nick_name.nil? || age.nil? || gender.nil?
   end
 end
